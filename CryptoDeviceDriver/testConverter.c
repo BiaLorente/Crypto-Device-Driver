03bb66b2a39f87227e3c480827ff725afaa0725b
Cesar Marrote Manzano <43895268+cesarmmanzano@users.noreply.github.com>
2020-09-23 09:32:22 -0300

    hexa para char

--- CryptoDeviceDriver/testConverter.c
+++ CryptoDeviceDriver/testConverter.c
@@ -5,13 +5,15 @@
 int main()
 {
 
-    char messageChar[] = "oacerdspmn";
-    char messageHexa[(2 * strlen(messageChar)) + 1]; //6f 61 63 65 72 64 73 70 6d 6e
+    char messageChar[] = "oededfsafsd";
+    char messageChar2[strlen(messageChar)];
+    char messageHexa[(2 * strlen(messageChar)) + 1];
     messageHexa[strlen(messageHexa) - 1] = '\0';
 
     int i = 0; //Controla char
     int j = 0; //Controla hexa
 
+    //Char para hexa
     while (messageChar[i] != '\0')
     {
         sprintf((messageHexa + j), "%02x", messageChar[i]);
@@ -20,6 +22,22 @@ int main()
     }
 
     printf("%s", messageHexa);
+    printf("\n");
+
+    //Hexa para char
+    i = 0;
+    j = 0;
+    char temp[3];
+    int num;
+    for (j = 0; j < strlen(messageHexa) + 1; j += 2)
+    {
+        sprintf(temp, "%c%c", messageHexa[j], messageHexa[j + 1]);
+        num = (int)strtol(temp, NULL, 16);
+        messageChar2[i] = (char)num;
+        i++;
+    }
+
+    printf("%s", messageChar2);
 
     return 0;
 }
